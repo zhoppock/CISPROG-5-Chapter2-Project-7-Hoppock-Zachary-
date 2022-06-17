c60f3bce54e65ccd001b33258550190d1acd6b14
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-17 16:38:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# We are going to calculate how many minutes occur in a year:
+
+# Initialize the following variables:
+# Days of a year = 365
+# Hours in a day = 24
+# Minutes in an hour = 60
+daysOfYear = 365
+hoursOfDay = 24
+minutesOfHour = 60
+
+# Compute the number of minutes in a year using the formula:
+# Minutes in a year = Days of a year * Hours in a day * Minutes in an hour
+minutesOfYear = daysOfYear * hoursOfDay * minutesOfHour
+
+#Print the minutes in a year
+print("There are " + str(minutesOfYear) + " minutes in a year.")
